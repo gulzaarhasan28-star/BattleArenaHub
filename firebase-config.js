5cede9865dd92a5d36b5673e5503060b41831d3b
gulzaarhasan28-star <gulzaarhasan28@gmail.com>
2026-01-18 15:53:45 +0530

    Clean project and keep final working files
    Removed old files and committed final stable version

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,9 @@
-<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-auth.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-firestore.js"></script>
-
-<script>
+// firebase-config.js
 const firebaseConfig = {
   apiKey: "AIzaSyDyXFlrTwnkUGFm9FPTw6lv6es7Uu5YhU4",
   authDomain: "battlearenahub-e8377.firebaseapp.com",
   projectId: "battlearenahub-e8377",
-  storageBucket: "battlearenahub-e8377.firebasestorage.app",
+  storageBucket: "battlearenahub-e8377.appspot.com",
   messagingSenderId: "183863662072",
   appId: "1:183863662072:web:e06705f1d7415cc3ceb20d"
 };
@@ -16,9 +12,8 @@ if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
-// GLOBAL EXPORTS (VERY IMPORTANT)
 window.auth = firebase.auth();
 window.db = firebase.firestore();
+
 window.provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
-</script>
