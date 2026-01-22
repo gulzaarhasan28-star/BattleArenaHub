fd13d18d4d2e0240f512e076f0251ee825561c97
gulzaarhasan28-star <gulzaarhasan28@gmail.com>
2026-01-22 10:20:16 +0530

    Update Firebase connection log message

--- firebase-config.js
+++ firebase-config.js
@@ -17,4 +17,4 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
-console.log("🔥 Firebase Connected Successfully");
+console.log("🔥 Firebase Connected");
